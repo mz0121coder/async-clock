fc60c217843ab05b472bf00d6436d712ce9b5199
Mohammad Zafar <mzcoder7@gmail.com>
2023-03-23 16:26:50 +0000

    replace image, add minute & hour hand

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
 /*
-Make clock hand move in interval of a second at a time
-Define let count = 0
-Use setInterval to move the hand
-    make anon function to count++
-    hand.style.transform = "rotate(6deg)"
-    set interval timer to 1sec (1000ms)
+In this challenge, you will be using your newfound knowledge of asynchronous code to control time and make the hand of this clock tick. Break down the problem, make a plan, and then translate it into code.
 
+- You should use setInterval.
+- The hand should move around the clock every second and should complete a full revolution in 60 seconds.
+- You should adjust the rotation of the hand using the transform property as demonstrated in `main.js`.
 
+## Bonus after your clock is ticking
 
-
-
-
+- Use the JS Date object to get the real seconds.
+- Add a minute hand.
+- Add an hour hand.
 */
 
+const hand = document.querySelector('.hand');
+let countdeg = 0;
 
-
-
-const hand = document.querySelector(".hand");
-let countdeg = 0
-//countdeg is the position of the hand. Each run of the function moves the hand 6 degrees.
-setInterval(function (){countdeg+=6;hand.style.transform = `rotate(${countdeg}deg)`;},1000)
-
-// setInterval(function (){hand.style.transform = `rotate(6deg)`;},1000)
-
-// hand.style.transform = `rotate(90deg)`; //🔍 HINT: THis is an example of how to rotate the hand!
+setInterval(function () {
+	countdeg += 6;
+	hand.style.transform = `rotate(${countdeg}deg)`;
+}, 1000);
