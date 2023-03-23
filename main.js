13a8ffcebc542683f3a49490354e541bc8d3cc3f
Mohammad Zafar <mzcoder7@gmail.com>
2023-03-23 17:39:57 +0000

    change div class, show mins + hours

--- main.js
+++ main.js
@@ -12,10 +12,22 @@ In this challenge, you will be using your newfound knowledge of asynchronous cod
 - Add an hour hand.
 */
 
-const hand = document.querySelector('.hand');
+const secondsHand = document.querySelector('.seconds');
+const minutesHand = document.querySelector('.minutes');
+const hoursHand = document.querySelector('.hours');
 let countdeg = 0;
 
 setInterval(function () {
-	countdeg += 6;
-	hand.style.transform = `rotate(${countdeg}deg)`;
+	const now = new Date();
+	const seconds = now.getSeconds();
+	const secondsDegrees = (seconds / 60) * 360;
+	secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+	const mins = now.getMinutes();
+	const minsDegrees = (mins / 60) * 360;
+	minutesHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+	const hours = now.getHours();
+	const hoursDegrees = (hours / 12) * 360;
+	hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }, 1000);
